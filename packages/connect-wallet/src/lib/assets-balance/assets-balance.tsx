e1252d04f29418a7f4a874e4258e9291ca616ee2
Nuno Araújo <nunofloaraujo@gmail.com>
2022-09-19 11:35:15 +0100

    Upped version and removed load more logic from assets balance list

--- packages/connect-wallet/src/lib/assets-balance/assets-balance.tsx
+++ packages/connect-wallet/src/lib/assets-balance/assets-balance.tsx
@@ -1,12 +1,17 @@
-import { Ref, useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import {
+  Ref,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import { useAccount, useNetwork } from 'wagmi';
 import TokenListItem, { itemHeight } from './token-list-item/token-list-item';
 import VirtualList from 'rc-virtual-list';
 import { Alert, Input, InputRef, List, Space } from 'antd';
 import { getTokens, Token } from './tokens';
 import { ConnectWalletContext } from '../connect-wallet-provider/connect-wallet-context';
-
-const itemsToLoad = 20;
 
 export const unsupportedChainErrorMsg =
   'Unsupported chain. Please select another chain.';
@@ -19,7 +24,6 @@ export function AssetsBalance() {
 
   const [tokens, setTokens] = useState<Token[]>();
   const [loadingTokens, setLoadingTokens] = useState(false);
-  const [lastIndex, setLastIndex] = useState(itemsToLoad);
   const [filter, setFilter] = useState('');
   const searchInputRef = useRef<InputRef>();
 
@@ -36,19 +40,8 @@ export function AssetsBalance() {
   );
 
   const data = useMemo(
-    () =>
-      filteredTokens
-        .slice(0, filteredTokens.length - 1 < lastIndex ? lastIndex : undefined)
-        .sort((a, b) => a.symbol.localeCompare(b.symbol)),
-    [lastIndex, filteredTokens]
-  );
-
-  const onScroll = useCallback(
-    (e: React.UIEvent<HTMLElement, UIEvent>) =>
-      e.currentTarget.scrollHeight - e.currentTarget.scrollTop <=
-        Math.round(itemHeight * itemsToShow) &&
-      setLastIndex(data.length + itemsToLoad),
-    [data, itemsToShow]
+    () => filteredTokens.sort((a, b) => a.symbol.localeCompare(b.symbol)),
+    [filteredTokens]
   );
 
   useEffect(() => {
@@ -79,7 +72,7 @@ export function AssetsBalance() {
               <VirtualList
                 height={itemHeight * itemsToShow}
                 itemKey={(token) => token.address}
-                {...{ data, itemHeight, onScroll }}
+                {...{ data, itemHeight }}
               >
                 {(token) => <TokenListItem {...{ token }} />}
               </VirtualList>
